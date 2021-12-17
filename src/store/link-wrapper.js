9be544521c1e5a92db78030128e18d9954c87e40
Jhonatas Machado <jhonatasrm@gmail.com>
2021-12-17 19:54:54 -0300

    updates header

--- src/store/link-wrapper.js
+++ src/store/link-wrapper.js
@@ -2,6 +2,6 @@ import React from '../../node_modules/react';
 import { NavLink } from '../../node_modules/react-router-dom';
 
 const LinkWrapper = (props) => (
-  <NavLink activeStyle={{ fontWeight: 'bold', color: '#FFFFFF', textDecoration: 'underline' }} {...props} />
+  <NavLink activeStyle={{ fontWeight: 'bold', color: '#FFFFFF', textDecoration: 'none' }} {...props} />
 );
 export default LinkWrapper;
